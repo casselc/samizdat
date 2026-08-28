cbb8f314fc09d37e9a613a7891f4febb6fce9181
Chuck Cassel <619504+casselc@users.noreply.github.com>
2026-08-28 05:20:11 -0700

    feat(instrumentation): select semantic function entries

--- test/samizdat/aspect_manifest_test.clj
+++ test/samizdat/aspect_manifest_test.clj
@@ -11,6 +11,17 @@
    "META-INF/jolt/aspects/samizdat-m2-embed.edn"
    "META-INF/jolt/aspects/samizdat-m2-http.edn"])
 
+(defn- qualified-symbol? [value]
+  (and (symbol? value) (some? (namespace value))))
+
+(defn- valid-selector? [match]
+  (and (pos-int? (:arity match))
+       (or (and (= #{:entry :arity} (set (keys match)))
+                (qualified-symbol? (:entry match)))
+           (and (= #{:ns :call :arity} (set (keys match)))
+                (symbol? (:ns match))
+                (qualified-symbol? (:call match))))))
+
 (deftest library-supplies-inert-instrumentation-contracts
   (doseq [resource-name manifest-resources]
     (testing resource-name
@@ -23,8 +34,25 @@
         (is (seq (:aspects manifest)))
         (doseq [aspect (:aspects manifest)]
           (is (keyword? (:id aspect)))
-          (is (symbol? (get-in aspect [:match :ns])))
-          (is (symbol? (get-in aspect [:match :call])))
-          (is (pos-int? (get-in aspect [:match :arity])))
+          (is (valid-selector? (:match aspect)))
           (is (keyword? (:advice-role aspect)))
           (is (= 1 (get-in aspect [:expect :matches]))))))))
+
+(deftest semantic-lifecycles-match-definition-entries
+  (let [read-manifest #(some-> % io/resource slurp edn/read-string)
+        core (read-manifest "META-INF/jolt/aspects/samizdat-m2-core.edn")
+        embed (read-manifest "META-INF/jolt/aspects/samizdat-m2-embed.edn")
+        by-id (into {} (map (juxt :id :match))
+                    (concat (:aspects core) (:aspects embed)))]
+    (is (= {:entry 'samizdat.agent.beam/run! :arity 1}
+           (get by-id :samizdat.embed/beam-run)))
+    (is (= {:entry 'samizdat.agent.beam/advance-branch :arity 3}
+           (get by-id :samizdat.agent.beam/turn)))
+    (is (= {:entry 'samizdat.llm.client/chat :arity 4}
+           (get by-id :samizdat.agent.infer/model)))
+    (is (= {:entry 'samizdat.agent.tools/run-tool :arity 1}
+           (get by-id :samizdat.agent.loop/tool)))
+    (is (= {:ns 'samizdat.llm.client
+            :call 'jolt.http-client/post
+            :arity 2}
+           (get by-id :samizdat.llm.client/http-post)))))
